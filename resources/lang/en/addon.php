fd09c61a20a0242e1fb42fe55a10486bf636f08b
Ryan Thompson <service@aiwebsystems.com>
2015-08-02 10:36:37 -0500

    Name / description

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Starter',
+    'name'        => 'Starter Theme',
+    'description' => 'A starter theme based on the Freelance Theme from <a href="http://startbootstrap.com">http://startbootstrap.com</a>.'
+];
